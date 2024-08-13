ed4c56038d178091e958b08f711c726f8000bee5
Administrator <Administrator@172.16.51.4>
2024-08-13 06:55:41 -0700

    This is case study commit

---
+++ src/com/statements/Hellodemo.java
@@ -0,0 +1,16 @@
+package com.statements;
+
+public class Hellodemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int a=10,b=20;
+		while(a<b)
+		{
+			System.out.println("Hello");
+		}
+		System.out.println("World");
+
+	}
+
+}
---
+++ src/com/statements/Jump_Statement.java
@@ -0,0 +1,21 @@
+package com.statements;
+
+public class Jump_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		int y=0;
+		for(;y<10;++y)
+		{
+			if(y%x==0)
+				continue;
+			else if(y==8)
+				break;
+			else
+				System.out.print(y+" ");
+		}
+
+	}
+
+}
